da6d732476da4c647def54252f444242c0b1bb8e
Pierre Bergue <pierre.bergue@gmail.com>
2021-02-17 12:03:59 +0100

    added axios

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = props => {
-  return (
-    <Auxiliary>
-      <Backdrop show={props.purchasing} exit={props.exitModal}/>
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.purchasing ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.purchasing ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
-    </Auxiliary>
-  );
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.purchasing !== this.props.purchasing;
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <Backdrop show={this.props.purchasing} exit={this.props.exitModal}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.purchasing ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.purchasing ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Auxiliary>
+    );
+  }
 };
 
-export default modal;
+export default Modal;
